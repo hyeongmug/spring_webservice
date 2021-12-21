8573eed3b7218a511ddb4989b10af381d2b6be3d
skygudanr <skygudanr@mobigen.com>
2021-12-21 15:30:06 +0900

    2일차 PostsRepository 인터페이스 작성 - JpaRepository<Entity클래스, PK타입> 상속 개념 이해 PostsRepositoryTest -> 게시글_불러오기 메서드 작성 및 실행.

--- src/main/java/com/skygudanr/webservice/domain/posts/PostsRepository.java
+++ src/main/java/com/skygudanr/webservice/domain/posts/PostsRepository.java
@@ -1,4 +1,13 @@
 package com.skygudanr.webservice.domain.posts;
 
-public interface PostsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/*
+보통 ibatis/MyBatis 등에서 Dao라고 불리는 DB Layer 접근자
+JPA에선 Repository라고 부르며 인터페이스로 생성
+단순히 인터페이스 생성후, JpaRepository<Entity클래스, PK타입>를 상속하면 기본적인 CRUD메소드가 자동 생성됨
+@Repository를 추가할 필요도 없음
+ */
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
 }
