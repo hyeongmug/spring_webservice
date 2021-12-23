55fbbd8e9287a4be8335bf7d370b540e08365667
skygudanr <skygudanr@mobigen.com>
2021-12-23 15:43:59 +0900

    4일차 게시글 등록 화면 구현 ==> bootstrap 사용

--- src/main/java/com/skygudanr/webservice/web/WebRestController.java
+++ src/main/java/com/skygudanr/webservice/web/WebRestController.java
@@ -1,7 +1,7 @@
 package com.skygudanr.webservice.web;
 
-import com.skygudanr.webservice.domain.posts.PostsRepository;
 import com.skygudanr.webservice.domain.posts.PostsSaveRequestDto;
+import com.skygudanr.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,7 +20,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
     }
 }
