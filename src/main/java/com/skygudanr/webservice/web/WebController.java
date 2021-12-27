f807525a0327f52b00129de6aa49cbf6bc88ae4e
skygudanr <skygudanr@mobigen.com>
2021-12-27 10:53:32 +0900

    4일차 게시글 목록 가져오기 => WebController.java 수정

--- src/main/java/com/skygudanr/webservice/web/WebController.java
+++ src/main/java/com/skygudanr/webservice/web/WebController.java
@@ -1,15 +1,20 @@
 package com.skygudanr.webservice.web;
 
+import com.skygudanr.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @AllArgsConstructor
 public class WebController {
 
+    private PostsService postsService;
+
     @GetMapping("/")
-    public String main() {
+    public String main(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "main";
     }
 
