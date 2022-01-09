213dbbcfec2ab8c11d1f0ea9295cf51105b816b6
skygudanr <skygudanr@mobigen.com>
2022-01-09 18:19:47 +0900

    9일차 nginx 무중단 배포

--- src/main/java/com/skygudanr/webservice/web/WebRestController.java
+++ src/main/java/com/skygudanr/webservice/web/WebRestController.java
@@ -3,16 +3,20 @@ package com.skygudanr.webservice.web;
 import com.skygudanr.webservice.dto.posts.PostsSaveRequestDto;
 import com.skygudanr.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -22,5 +26,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
